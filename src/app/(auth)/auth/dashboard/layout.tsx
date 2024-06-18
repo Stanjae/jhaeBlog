5c0b9d9033bf6bf2698418f6ddded5c310d9841b
stanjae <stanykhay29@gmail.com>
2024-06-18 23:32:09 +0100

    lop

--- src/app/(auth)/auth/dashboard/layout.tsx
+++ src/app/(auth)/auth/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import DashboardSidebar from '@/app/ui/navigation/DashboardSidebar';
+import { MobileDashNav } from '@/app/ui/navigation/MobileDashNav';
 import React from 'react'
 
 const layout = ({
@@ -6,10 +8,22 @@ const layout = ({
     children: React.ReactNode;
   }>) => {
   return (
-    <div className=' bg-slate-800 relative flex flex-1 h-screen'>
-      <div className=' z-50 w-[25%] fixed left-0 top-0 bg-orange-600'></div>
-      <div className=' ml-[25%] flex-grow bg-green-400'>
-        {children}
+    <div className='relative p-1 overflow-hidden flex min-h-dvh'>
+      <div className=' h-full bg-red-400 w-[24%] border-r-bgdark hidden md:block fixed left-0 top-0 '>
+        <DashboardSidebar/>
+      </div>
+      <div className=' md:ml-[24%] bg-gray-800 px-4 flex-1 relative'>
+        <div className=' shadow-lg h-14 fixed w-full md:w-[76%] right-0 top-0 z-10 bg-white border-b-bgdark '>
+          <div className=' md:hidden block relative z-20'>
+            <MobileDashNav/>
+          </div>
+          
+        </div>
+        <div className=' mt-24'>
+          {children}
+        </div>
+        
+        
       </div>
     </div>
   )
